c75f171c322a5a98d89e5eda8ea4880f2a1b13de
Fabien Loup <info@norydev.com>
2016-11-23 10:24:45 +0100

    reviews for restaurants

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = @restaurant.reviews.build(review_params)
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
+
+    # @review = Review.new(review_params)
+    # @review.restaurant = @restaurant
+    # @review.save
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:content)
+    end
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+end
